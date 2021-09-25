f9a02adcfdd6a94c73776724897ceae04b785377
L0GAN9 <89943160+L0GAN9@users.noreply.github.com>
2021-09-25 16:14:02 +0000

    Details added

--- main.cs
+++ main.cs
@@ -8,9 +8,9 @@ class Program {
     
     Console.WriteLine("Enter a Number");
     int x = Convert.ToInt32(Console.ReadLine());
-    for (int num = 1; num < x; ++num)
+    for (int num = 1; num < x; ++num) // Conditions and Loop
     {
-      if (x%num == 0)
+      if (x%num == 0) // Conditons to be met for a Non Prime Number
       {
        Console.WriteLine("Number is Not Prime");
        {
@@ -18,7 +18,7 @@ class Program {
        }
       }
       
-      if (x%num != 0)
+      if (x%num != 0) // Condition to be met for a Prime Number
       {
         Console.WriteLine("Number is Prime");
         {
